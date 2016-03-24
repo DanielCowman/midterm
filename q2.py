20313fee3625c841897d0bf28ee0e3028a61bcd7
DanielCowman <dcowman@mail.valenciacollege.edu>
2016-03-23 22:46:16 -0400

    midterm

--- q2.py
+++ q2.py
@@ -1,4 +1,5 @@
-# This is a very simple game of sticks. There are 21 sticks, first the user picks number of sticks between 1-4, then the computer picks sticks(1-4). Who ever will pick the last stick will lose.
+# This is a very simple game of sticks. There are 21 sticks, first the user picks number of sticks between 1-4,
+# then the computer picks sticks(1-4). Who ever will pick the last stick will lose.
 # Look for the TODO blocks as an indication of when you have to add your own code.
 
 import random
@@ -22,15 +23,27 @@ def playGame():
         
 
 def askUserChoice():
-    # TODO: write code in this functiont that:
+    userInput = input("\t Number: ")
+    while userInput > 4 or userInput < 1:
+        print("Please enter a number between 1 and 4:")
+        userInput = input("\t Number: ")
+    return int(userInput)
+
+    # TODO: write code in this function that:
     # 1. Asks the user to enter their input (between 1 and 4)
-    # 2. Checks that the user's input is valid. If it's not valid (if it's not between 1 and 4), then ask the user to re-enter their input.
+    # 2. Checks that the user's input is valid.
+    # If it's not valid (if it's not between 1 and 4), then ask the user to re-enter their input.
     # 3. Once the user enters a valid input, return that input as an integer.
 
 
 def subtractSticks( number ):
-    global sticks 
-    
+    global sticks
+    sticks = sticks - number
+
+    if sticks <= 1:
+        return True
+    else:
+        return False
     # TODO: write code inside this function that:
     # 1. subtracts the parameter `number` from the global variable `sticks`
     # 2. checks if the number subtracted resulted in the last stick, if so, return True
@@ -38,3 +51,4 @@ def subtractSticks( number ):
     
 def determineComputerChoice():
     # TODO: write code inside this function that returns an integer between 1 and 4, random chosen by the computer
+    return random.randrange(1,5)
